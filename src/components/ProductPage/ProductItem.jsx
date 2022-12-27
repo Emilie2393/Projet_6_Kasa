97fbd55dbabe90fb408068ba1c1440593f73ba84
Emilie <emilie.rich4rd@gmail.com>
2022-12-27 16:38:43 +0100

    suppression loading productitem

--- src/components/ProductPage/ProductItem.jsx
+++ src/components/ProductPage/ProductItem.jsx
@@ -5,17 +5,11 @@ import Gallery from "./Gallery"
 import Rating from "./ProductRating"
 import { useEffect } from "react";
 import { useFetch } from "../PageComponents/Fetch";
-import Logo from "../../assets/LOGO.png"
 
 
-function ProductItem({modifyPage}) {
+function ProductItem({ modifyPage }) {
   // retour de useFetch à travers les constantes //
-  const { isLoading, data, error } = useFetch(
-    `../data/List.json`
-  );
-
-  // test //
-  console.log("les datas retournées", data);
+  const { isLoading, data, error } = useFetch(`../data/List.json`);
 
   // récupère l'id du logement dans l'url //
   const { id } = useParams();
@@ -29,19 +23,16 @@ function ProductItem({modifyPage}) {
   useEffect(() => {
     // quand le chargement est terminé et que les data sont disponibles //
     if (!isLoading) {
-      modifyPage(false)
-      // test //
-      console.log("page produit : useeffect")
+      modifyPage(false);
       // en cas de changement de l'id du produit dans l'URL, affiche la page du bon produit ou redirige vers la page d'erreur //
       const idSearch = idList.find((nb) => nb === id);
       if (idSearch === undefined) {
         // renvoie vers 404 //
         navigate("*");
       }
-    }
-    else{
+    } else {
       // page modifiée pendant le chargement //
-      modifyPage(true)
+      modifyPage(true);
     }
   });
 
@@ -50,62 +41,57 @@ function ProductItem({modifyPage}) {
   }
 
   // si les données chargent, affiche un message en attendant puis affiche le logement //
-  return isLoading ? (
-    
-    <div className="loading">
-        <h1>Le logement arrive . . .</h1>
-        <img className="loading__image" src ={Logo} alt="logo kasa" />
-    </div>
-    
-  ) : (
+  return (
     <div>
-      {flatInfos.map(
-        ({
-          id,
-          title,
-          rating,
-          description,
-          equipments,
-          pictures,
-          location,
-          tags,
-          host,
-        }) => (
-          <div key={id} className="kaza__product">
-            <Gallery pictures={pictures} />
-            <div className="kaza__product__flex">
-              <div>
-                <h1 className="kaza__product__title">{title}</h1>
-                <div className="kaza__product__location">{location}</div>
-                <div className="kaza__product__tags">
-                  {tags.map((tag, index) => (
-                    <span key={index}>{tag}</span>
-                  ))}
-                </div>
-              </div>
-              <div className="kaza__product__hostandrating">
-                <div className="kaza__product__host">
-                  <div className="kaza__product__host__name">
-                    <span>{host.name.split(" ")[0]}</span>
-                    <span>{host.name.split(" ")[1]}</span>
+      <div>
+        {flatInfos.map(
+          ({
+            id,
+            title,
+            rating,
+            description,
+            equipments,
+            pictures,
+            location,
+            tags,
+            host,
+          }) => (
+            <div key={id} className="kaza__product">
+              <Gallery pictures={pictures} />
+              <div className="kaza__product__flex">
+                <div>
+                  <h1 className="kaza__product__title">{title}</h1>
+                  <div className="kaza__product__location">{location}</div>
+                  <div className="kaza__product__tags">
+                    {tags.map((tag, index) => (
+                      <span key={index}>{tag}</span>
+                    ))}
                   </div>
-                  <img src={host.picture} alt="visage du propriétaire" />
                 </div>
-                <Rating rating={rating} />
+                <div className="kaza__product__hostandrating">
+                  <div className="kaza__product__host">
+                    <div className="kaza__product__host__name">
+                      <span>{host.name.split(" ")[0]}</span>
+                      <span>{host.name.split(" ")[1]}</span>
+                    </div>
+                    <img src={host.picture} alt="visage du propriétaire" />
+                  </div>
+                  <Rating rating={rating} />
+                </div>
               </div>
-            </div>
 
-            <div className="kaza__product__collapse">
-              <div className="kaza__product__collapse__width">
-                <Collapse title={"description"} text={description} />
-              </div>
-              <div className="kaza__product__collapse__width">
-                <Collapse title={"équipements"} textArray={equipments} />
+              <div className="kaza__product__collapse">
+                <div className="kaza__product__collapse__width">
+                  <Collapse title={"description"} text={description} />
+                </div>
+                <div className="kaza__product__collapse__width">
+                  <Collapse title={"équipements"} textArray={equipments} />
+                </div>
               </div>
             </div>
-          </div>
-        )
-      )}
+          )
+        )}
+      </div>
     </div>
   );
 }
